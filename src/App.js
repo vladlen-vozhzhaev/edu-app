a5dd810ecf306a182fc1b4f7ebf2f4e751725fb4
Vladlen <vladlen@vozhzhaev.ru>
2020-09-30 11:02:57 +0300

    Первый коммит

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, NavLink, Route} from "react-router-dom";
 
+const Profile = ()=>{
+    return (
+        <h1>Это страница с профилем</h1>
+    )
+}
+const Message = ()=>{
+    return <h1>Страница с сообщениями</h1>
+}
+const Settings = ()=>{
+    return <h1>Страница с настройками</h1>
+}
+const Friends = ()=>{
+    return <h1>Страница с друзьями</h1>
+}
+
+const Menu = ()=>{
+    return <div className="nav flex-column nav-pills" aria-orientation="vertical">
+        <NavLink className="nav-link" to="profile">Профиль</NavLink>
+        <NavLink className="nav-link" to="message">Сообщения</NavLink>
+        <NavLink className="nav-link" to="settings">Настройки</NavLink>
+        <NavLink className="nav-link" to="friends">Мои друзья</NavLink>
+    </div>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+          <BrowserRouter>
+            <div className="row">
+              <div className="col-sm-3">
+                <Menu/>
+              </div>
+              <div className="col-sm-9">
+                <Route path="/profile" component={Profile}/>
+                <Route path="/message" component={Message}/>
+                <Route path="/settings" component={Settings}/>
+                <Route path="/friends" component={Friends}/>
+              </div>
+            </div>
+          </BrowserRouter>
+      </div>
   );
 }
 
